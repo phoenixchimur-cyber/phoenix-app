cf6920e0ee30be1a7f8ecff702b364e61c60188f
Prashant Chide <phoenixchimur@email.com>
2026-03-20 10:20:14 +0530

    fix port issue

--- app.py
+++ app.py
@@ -260,7 +260,7 @@ def logout():
     return redirect('/')
 
 # ================= RUN =================
-if __name__=='__main__':
+if __name__ == '__main__':
     import os
-    port=int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
